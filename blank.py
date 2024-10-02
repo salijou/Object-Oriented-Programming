ae082501e033460d12f1996bcb21802a8d5991e8
ZeinebKarmani <eaa24zeka@students.eaaa.dk>
2024-10-02 14:58:30 +0200

    blank file

---
+++ blank.py
@@ -0,0 +1,27 @@
+class Class:
+
+    def __init__(self):
+
+        self.att1 =1      #attribut1
+        self.att2 = 2     #attribut2
+        self.att3 = 3     #attribut3
+        self.att4 = 4     #attribut4
+        self.att5 =  5    #attribut5
+
+
+    def meth1(self):      #method1
+        print("meth1")
+
+    def meth2(self):      #method2
+        if self.att1 < 20:
+            print("low")
+        else:
+            print("high")
+    def meth3(self):      #method3
+        print("")
+
+    def meth4(self):      #method4
+        print("")
+
+
+
---
+++ blanktest.py
@@ -0,0 +1,19 @@
+from blank_class import Class
+
+obj1=Class() #instance1
+obj2=Class() #instance2
+
+obj1.att1=25
+obj1.att2=10
+obj1.att3=60
+
+obj2.att1=12
+obj2.att2=20
+
+obj1.meth1()
+obj1.meth2()
+print (obj1.att1)
+
+obj2.meth1()
+obj2.meth2()
+print(obj2.att1)
